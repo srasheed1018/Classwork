e8a7c6b1b59627ac7c624291d7bffd35289ae36e
BT_1N3_26 <BT_1N3_26@BT_1N3_26.bktech.local>
2017-09-13 11:03:12 -0400

    fixed message spacing

--- JavaClasswork/src/intro/IntroMain.java
+++ JavaClasswork/src/intro/IntroMain.java
@@ -5,14 +5,14 @@ public class IntroMain {
 
 	
 	//constants (public)
-	public static final String[] DESCRIPTIONS = {" is a teacher", "is a student", "is a human being", "is imaginary"};
+	public static final String[] DESCRIPTIONS = {" is a teacher", " is a student", " is a human being", " is imaginary"};
 	
 	
 	public static void main(String[] args) {
 		//declaration of a local variable
 		//notice the use of the word 'new'
 		//new must be used to call a constructor
-		CodingConventions conventionsInstance = new CodingConventions("Mr. Nockles", 1);
+		CodingConventions conventionsInstance = new CodingConventions("Mr. Nockles", 2);
 	
 		//instance method call
 		conventionsInstance.doStuff();
