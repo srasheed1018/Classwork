bff99693e8dc8003bfcedabd7d253100e7a44f0b
BT_1N3_26 <BT_1N3_26@BT_1N3_26.bktech.local>
2017-10-19 11:44:13 -0400

    10-19-17 (didnt finish testShuffling)

--- JavaClasswork/src/warmup_10_18_17/Person.java
+++ JavaClasswork/src/warmup_10_18_17/Person.java
@@ -13,15 +13,93 @@ public class Person {
 	private String firstName;
 	private String lastName;
 	private Borough home;
+	private Hobby hobby;
+	private Person[] friends;
 
 	public Person (String first, String last, Borough home)
 	{
 		this.firstName = first;
 		this.lastName = last;
 		this.home = home;
+		this.hobby = Hobby.randomHobby();
+		friends = new Person[3];
 	}
+	
+	//chooses friends from People based on whom is
+	//of the same class as this instance and who has the same hobbies
+	public void mingle(Person[] people)
+	{
+		for (Person p:people)
+		{
+			if (p != this)
+			{
+				//reassign p to the better of the two friends:
+				//your current best friend or p
+				p = betterFriend(p, friends[0]);
+				
+				addFriendToFirstPlace(p);
+			}
+		}
+	}
+	
+	private Person betterFriend(Person p, Person q) {
+		if (p==null)
+		{
+			return q;
+		}
+		if (q==null)
+		{
+			return p;
+		}
+		if (p.getClass() == q.getClass() && q.getClass() == this.getClass())
+		{
+			if (p.hobby == this.hobby)
+			{
+				return p;
+			}
+			if (q.hobby == this.hobby)
+			{
+				return q;
+			}
+			return p;
+		}
+		else 
+		{
+			if (p.getClass()== this.getClass())
+			{
+				return p;
+			}
+			if (q.getClass() == this.getClass())
+			{
+				return q;
+			}
+		}
+		//if none of them these are true; just take p
+		return p;
+	}
+
+	public void printFriends()
+	{
+		System.out.println("My name is "+firstName+" "+lastName+" and these are my friends:");
+		for (Person f:friends)
+		{
+			if (f!=null) System.out.println(f);
+		}
+	}
+	
+	//moves all Persons in friends back one index and puts p at 0
+	private void addFriendToFirstPlace(Person p)
+	{
+		//goes backwards
+		for (int i=friends.length-1; i>0; i--)
+		{
+			friends[i] = friends[i-1];
+		}
+		friends[0] = p;
+	}
+	
 	public String toString()
 	{
-		return "My name is "+firstName+" "+lastName+" and I live in "+home+".";
+		return "My name is "+firstName+" "+lastName+" and I live in "+home+". I like to "+hobby;
 	}
 }
