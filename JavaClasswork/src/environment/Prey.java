6838502f036953629e091732f9a417a56607eb20
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2017-11-21 10:37:45 -0500

    cw 11-21-17

---
+++ JavaClasswork/src/environment/Prey.java
@@ -0,0 +1,15 @@
+package environment;
+
+public class Prey extends ReproductionAnimal {
+
+	public Prey(Habitat matingArea) {
+		super(matingArea);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
